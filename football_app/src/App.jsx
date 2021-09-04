c63db3e05a00a00012cae5213585b3d2e35285b8
secolema <secolema@iu.edu>
2021-09-03 22:12:20 -0500

    football

---
+++ football_app/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Football App
+        </h1>
+
+      </header>
+
+
+      <nav>
+
+
+
+        <div>
+          <h1>
+            
+          </h1>
+        </div>
+      </nav>
+
+
+    </div>
+  );
+}
+
+export default App;
